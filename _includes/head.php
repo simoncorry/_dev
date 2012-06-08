5a15fe59fe7a73d4b29d3b61dab02a76b766e0d2
simoncorry <simoncorry@gmail.com>
2012-06-08 16:20:11 -0400

    Head.php updated with new links

--- _includes/head.php
+++ _includes/head.php
@@ -7,7 +7,14 @@
         <link rel="apple-touch-icon-precomposed" href="/_media/images/id/apple-touch-icon-114x114-precomposed.jpg">
         <link rel="apple-touch-icon-precomposed" href="/_media/images/id/apple-touch-icon-144x144-precomposed.jpg">
         
-        <!-- /// MINIFY /// -->
-        <link type="text/css" rel="stylesheet" href="/min/b=_media/css&amp;f=reset.css,main.css,open.css" />
-        <script type="text/javascript" src="/min/b=_media/js&amp;f=jquery-1.7.1.min.js,modernizr-2.0.4.js,jquery.footer.js,links.js,jquery.scrollTo.js,jquery.localscroll.js,jquery.lifestream.js,jquery.timeago.js,lifestream.js"></script>
+        <!-- /// CSS /// -->
+        <link type="text/css" rel="stylesheet" href="/_media/css/reset.css" />
+        <link type="text/css" rel="stylesheet" href="/_media/css/main.css" />
+        <link type="text/css" rel="stylesheet" href="/_media/css/open.css" />
+        
+        <!-- /// JS /// -->
+        <script type="text/javascript" src="/_media/js/jquery-1.7.2.min.js"></script>
+        <script type="text/javascript" src="/_media/js/modernizr-2.5.3.min.js"></script>
+        <script type="text/javascript" src="/_media/js/raphael-2.1.0.min.js"></script>
+        <script type="text/javascript" src="/_media/js/links.js"></script>
 	
