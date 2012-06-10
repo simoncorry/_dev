d14a2e07b22c92da0b43ce5d023930d67da99fff
simoncorry <simoncorry@gmail.com>
2012-06-10 16:27:47 -0400

    cloud_small_1 added

--- _media/js/assets.js
+++ _media/js/assets.js
@@ -2,10 +2,39 @@
 
 /* Clouds */
 
-$(document).ready(function() {  
-    var paper = new Raphael(document.getElementById('cloud-small-1'), 200, 200);  
-    var cloud_small_one = paper.path('M 0 30 l 0 5 l 5 0 l 0 -5 l -5 0 z');
-    cloud_small_one.attr({fill: '#fff', stroke: '#eae6d8', 'stroke-width':1}); 
-  
+$(document).ready(function() {
+    /* Cloud Colors */
+    cloud_colors = ['#eae6d8','#fff'];
+ 
+    /* Cloud Small 1 */ 
+    var paper = new Raphael(document.getElementById('cloud-small-1'), 80, 45);
+    var cs_1_stroke = paper.path('M 0 30 l 0 5 l 5 0 l 0 5 l 10 0 l 0 -5 l 5 0 l 0 5 l 5 0 l 0 5 l 15 0 l 0 -5 l 10 0 l 0 5 l 15 0 l 0 -10 l 10 0 l 0 -5 l 5 0 l 0 -5 l -5 0 l 0 -5 l -5 0 l 0 -10 l -5 0 l 0 -5 l -5 0 l 0 5 l -5 0 l 0 5 l -5 0 l 0 -5 l -5 0 l 0 -5 l -5 0 l 0 -5 l -10 0 l 0 5 l -5 0 l 0 5 l -5 0 l 0 5 l -10 0 l 0 5 l -5 0 l 0 10 l -5 0 l 0 5 z');
+    cs_1_stroke.attr(
+        {
+            fill:cloud_colors[0],
+            stroke:'none'
+        }
+    );
+    var cs_1_inner = paper.path('M 5 30 l 0 5 l 10 0 l 0 -5 l 5 0 l 0 5 l 5 0 l 0 5 l 15 0 l 0 -5 l 10 0 l 0 5 l 15 0 l 0 -5 l 5 0 l 0 -5 l 5 0 l 0 -5 l -5 0 l 0 -5 l -5 0 l 0 -10 l -5 0 l 0 5 l -5 0 l 0 5 l -5 0 l 0 -5 l -5 0 l 0 -5 l -5 0 l 0 -5 l -10 0 l 0 10 l -10 0 l 0 5 l -10 0 l 0 10 z');
+    cs_1_inner.attr(
+        {
+            fill:cloud_colors[1],
+            stroke:'none'
+        }
+    );
+    var cs_1_details_1 = paper.path('M 25 25 l 10 0 l 0 10 l -5 0 l 0 -5 l -5 0 l 0 -5 z');
+    cs_1_details_1.attr(
+        {
+            fill:cloud_colors[0],
+            stroke:'none'
+        }
+    );
+    var cs_1_details_2 = paper.path('M 40 25 l 15 0 l 0 5 l 5 0 l 0 5 l -5 0 l 0 -5 l -15 0 l 0 -5 z');
+    cs_1_details_2.attr(
+        {
+            fill:cloud_colors[0],
+            stroke:'none'
+        }
+    );   
 });  
   
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8" />
         
         <?php /* ERROR */ ini_set('error_reporting', E_ALL); ini_set('display_errors', 0); ?>
-        <?php /* TAG */ include_once($_SERVER['DOCUMENT_ROOT']."/_includes/tag.php"); ?>
+        <?php /* TAG */ //include_once($_SERVER['DOCUMENT_ROOT']."/_includes/tag.php"); ?>
         <?php /* PAGE */ $thisPage="Home"; ?>
         
         <title>Science &amp; Creativity</title>
@@ -17,8 +17,8 @@
         <meta name="keywords" content="simon corry, science &amp; creativity, user experience design, information architecture, design &amp; development"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1.0"> 
                     
-        <?php /* FACEBOOK OG */ include_once($_SERVER['DOCUMENT_ROOT']."/_includes/fb-og.php"); ?>
-        <?php /* GOOGLE PLUS SNIPPET */ include_once($_SERVER['DOCUMENT_ROOT']."/_includes/gp-snippet.php"); ?>
+        <?php /* FACEBOOK OG */ //include_once($_SERVER['DOCUMENT_ROOT']."/_includes/fb-og.php"); ?>
+        <?php /* GOOGLE PLUS SNIPPET */ //include_once($_SERVER['DOCUMENT_ROOT']."/_includes/gp-snippet.php"); ?>
         <?php /* LINKS */ include_once($_SERVER['DOCUMENT_ROOT']."/_includes/head.php"); ?>  
              
         <!-- /// PRELOAD  /// -->
@@ -35,8 +35,7 @@
     </head>
     <body>   
         <div id="preloader"></div>   
-        <div id="container">
-        
+        <div id="container">       
             <section class="top">
         <?php /* OPEN */ include_once($_SERVER['DOCUMENT_ROOT']."/_includes/open.php"); ?>            
             </section>
