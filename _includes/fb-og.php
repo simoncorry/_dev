6474032662cfd9790eb056c27e31add008aa4c1f
simoncorry <simoncorry@gmail.com>
2013-05-01 11:30:06 -0400

    Cleansed directory for new project

--- _includes/fb-og.php
+++
@@ -1,9 +0,0 @@
-        
-        <!-- /// FACEBOOK OG /// -->
-        <meta name="" property="og:title" content="Simon Corry &raquo; Science &amp; Creativity"/>
-        <meta name="" property="og:type" content="company"/>
-        <meta name="" property="og:image" content="http://www.simoncorry.com/_media/images/id/facebook.jpg"/>
-        <meta name="" property="og:url" content="http://www.simoncorry.com/"/>
-        <meta name="" property="og:site_name" content="Simon Corry &raquo; Science &amp; Creativity"/>
-        <meta name="" property="og:description" content="The new home of science &amp; creativity."/>
-        <meta name="" property="fb:admins" content="512859885"/>
--- _includes/xmlns.php
+++
@@ -1,1 +0,0 @@
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml">
