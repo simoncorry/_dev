1f76247f67d132874807524e403fd03c4f26bba4
simoncorry <simoncorry@gmail.com>
2013-05-01 12:48:52 -0400

    fresh install, node.js installed test script added example.js

---
+++ includes/google-snippet.php
@@ -0,0 +1,5 @@
+
+        <!-- /// GOOGLE SNIPPET AND FRAME /// -->
+        <meta itemprop="name" content="*** NAME ***">
+        <meta itemprop="description" content="*** DESCRIPTION ***">
+        <meta itemprop="image" content="*** URL *** /assets/images/lib/icons/facebook-google.png">
