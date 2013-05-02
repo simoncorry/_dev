31446a14fb82f049e50abddd780646ff95bc1b69
simoncorry <simoncorry@gmail.com>
2013-05-02 11:09:05 -0400

    Serve static index file

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+// Node Dependencies
 var express = require('express');
 var app     = express();
 
-
 app.use(express.logger());
 
+// Basic Helo World Test
+/*
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+*/
 
-var port = process.env.PORT || 5000;
-
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-
-/*
+// Serve Static Files
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
-*/
+
+// Add Port And Listen
+var port = 1337;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
