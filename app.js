31314780da72e9905617e5c69cf9f517239064b9
simoncorry <simoncorry@gmail.com>
2013-05-06 23:10:03 -0400

    Google and Facebook snippets added, image lib added

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.get('/about', about.page);
 app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
