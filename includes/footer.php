8c0fcb04fcb0f0e3fdb000be743a50e667d435d6
simoncorry <simoncorry@gmail.com>
2013-12-16 17:32:11 -0500

    Switch to WP theme

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-
-        <!-- /// FOOTER /// -->
-        <section id="footer">
-            <button id="btn_remix">Remix Colors</button>
-            <button id="btn_view">
-                <div class="btn_bg"></div>
-            </button> 
-            <div class="clearfix"></div>
-        </section>
--- includes/google-plus.php
+++
@@ -1,4 +0,0 @@
-        
-        <!-- /// GOOGLE PLUS /// -->
-        <a href="https://plus.google.com/112943355702113622332?rel=author"    class="hidden"></a>
-        <a href="https://plus.google.com/112943355702113622332?rel=publisher" class="hidden"></a>
